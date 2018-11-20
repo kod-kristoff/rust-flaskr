d2c8a11936fa362de72bca16eedb020c7253138d
Kristoffer Andersson <kode.kristoff@gmail.com>
2018-11-20 16:32:23 +0100

    add hello route

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+extern crate thruster; 
+extern crate futures; 
+
+use std::boxed::Box; 
+use futures::future; 
+use thruster::{App, 
+	BasicContext as Ctx, 
+	MiddlewareChain, 
+	MiddlewareReturnValue, 
+	Request
+	}; 
+use thruster::builtins::server::Server; 
+use thruster::server::ThrusterServer; 
+
+fn hello(mut context: Ctx, 
+		 _chain: &MiddlewareChain<Ctx>) 
+		 -> MiddlewareReturnValue<Ctx> { 
+	let val = "Hello, World!".to_owned(); 
+	context.body = val; 
+
+	Box::new(future::ok(context)) 
+} 
+
+fn main() { 
+	println!("Starting server..."); 
+
+	let mut app = App::<Request, Ctx>::new(); 
+
+	app.get("/hello", vec![hello]); 
+
+	let server = Server::new(app); 
+	server.start("0.0.0.0", 4321); 
 }
