86b18813ae194191a24ec53e1e7c5646be288fc4
Kristoffer Andersson <kristoffer.andersson@gu.se>
2018-11-22 21:37:09 +0100

    initial code

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,75 @@
-extern crate thruster; 
-extern crate futures; 
+extern crate thruster;
+extern crate futures;
+extern crate serde;
+extern crate serde_json;
+extern crate smallvec;
+extern crate tokio;
+extern crate dotenv;
 
-use std::boxed::Box; 
-use futures::future; 
-use thruster::{App, 
-	BasicContext as Ctx, 
-	MiddlewareChain, 
-	MiddlewareReturnValue, 
-	Request
-	}; 
-use thruster::builtins::server::Server; 
-use thruster::server::ThrusterServer; 
+#[macro_use] extern crate diesel;
+#[macro_use] extern crate lazy_static;
 
-fn hello(mut context: Ctx, 
-		 _chain: &MiddlewareChain<Ctx>) 
-		 -> MiddlewareReturnValue<Ctx> { 
-	let val = "Hello, World!".to_owned(); 
-	context.body = val; 
+mod context;
+mod schema;
+mod content_model;
 
-	Box::new(future::ok(context)) 
-} 
+use futures::future;
 
-fn main() { 
-	println!("Starting server..."); 
+use thruster::{App, MiddlewareChain, MiddlewareReturnValue};
+use thruster::builtins::server::Server;
+use thruster::server::ThrusterServer;
+use context::{Ctx, generate_context};
+use diesel::prelude::*;
+use diesel::sqlite::SqliteConnection;
+use diesel::r2d2::{ConnectionManager, Pool};
+use schema::content::dsl::*;
+use dotenv::dotenv;
+use std::env;
 
-	let mut app = App::<Request, Ctx>::new(); 
+lazy_static! {
+    static ref db: Pool<ConnectionManager<SqliteConnection>> = {
+      dotenv().ok();
 
-	app.get("/hello", vec![hello]); 
+      let database_url = env::var("DATABASE_URL")
+        .expect("DATABASE_URL must be set");
 
-	let server = Server::new(app); 
-	server.start("0.0.0.0", 4321); 
+      let manager = ConnectionManager::<SqliteConnection>::new(database_url);
+      Pool::new(manager).expect("Error creating db pool")
+    };
+}
+
+fn fetch_value(mut context: Ctx, _chain: &MiddlewareChain<Ctx>) -> MiddlewareReturnValue<Ctx> {
+  let conn = db.get().unwrap();
+
+  let results = content
+    .limit(1)
+    .load::<content_model::Content>(&conn)
+    .unwrap();
+
+  let result = results.get(0).unwrap();
+  context.body = result.val.clone();
+
+  Box::new(future::ok(context))
+}
+
+fn not_found_404(mut context: Ctx, _chain: &MiddlewareChain<Ctx>) -> MiddlewareReturnValue<Ctx> {
+  context.body = "<html>
+  ( ͡° ͜ʖ ͡°) What're you looking for here?
+</html>".to_owned();
+  context.set_header("Content-Type", "text/html");
+  context.status_code = 404;
+
+  Box::new(future::ok(context))
+}
+
+fn main() {
+  println!("Starting server...");
+
+  let mut app = App::create(generate_context);
+
+  app.get("/plaintext", vec![fetch_value]);
+  app.get("/*", vec![not_found_404]);
+
+  let server = Server::new(app);
+  server.start("0.0.0.0", 4321);
 }
